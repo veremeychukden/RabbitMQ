365ee8cbb4066629b5a9d380473aa03306267f80
veremeychukden <veremeychukden@gmail.com>
2022-04-28 14:52:30 +0300

    RabbitMQ test project

---
+++ Receive/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Receive
+{
+    public static void Main()
+    {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                };
+                channel.BasicConsume(queue: "hello",
+                    autoAck: true,
+                    consumer: consumer);
+            }
+    }
+}
